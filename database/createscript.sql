b2638fe5860861ebfe46d021b2d41b3cda1fcbf3
Florian van der Meulen <344612@student.mboutrecht.nl>
2026-01-29 13:57:48 +0100

    Initial commit

--- database/createscript.sql
+++ database/createscript.sql
@@ -1,59 +1,58 @@
--- database
--- ----------------------------------------------------------------------
--- Stap : 01
--- Doel : Maak een nieuwe database aan: Rollercoaster_2509b
--- ----------------------------------------------------------------------
--- Versie Datum Autuer Omschrijving
--- ----------------------------------------------------------------------
--- -- 01 04-12-2025 F.Meulen Database met de hoogste
--- -- achtbanen van Europa
--- ----------------------------------------------------------------------
+-- step : 01
+-- **********************************************************
+-- doel: Maak een nieuwe database aan: Rollercoaster
+-- **************************************************
+-- Versie       Datum       Auteur        Omschrijving
+-- ******       *****       ******        ************
+-- 01           04-12-2025  A.Akoudad     Database met
+-- de hoogste achtbanen van Europa
+-- ***********************************************************
 
--- Verwijder database Rollercoaster_2509b
-DROP DATABASE IF EXISTS Rollercoaster_2509b;
+-- Verwijder de database als deze al bestaat
+DROP DATABASE IF EXISTS Rollercoaster;
 
--- Maak de database Rollercoaster_2509b
-CREATE DATABASE Rollercoaster_2509b;
+-- Maak de database aan
+CREATE DATABASE Rollercoaster;
 
--- Gebruik de database Rollercoaster_2509b
-USE Rollercoaster_2509b;
+-- Gebruik de nieuw aangemaakte database
+USE Rollercoaster;
 
+-- step : 02
+-- **********************************************************
+-- doel: Maak een nieuwe tabel aan: Rollercoaster
+-- **************************************************
+-- Versie       Datum       Auteur        Omschrijving
+-- ******       *****       ******        ************
+-- 01           04-12-2025  A.Akoudad     tabel met
+--                                        de hoogste achtbanen
+--                                        van Europa
+-- ***********************************************************
 
--- Stap : 02
--- Doel : Maak een nieuwe tabel aan: Rollercoaster
--- ----------------------------------------------------------------------
--- Versie Datum Autuer Omschrijving
--- ----------------------------------------------------------------------
--- -- 01 04-12-2025 F.Meulen Tabel met de hoogste
--- -- achtbanen van Europa
--- ----------------------------------------------------------------------
-
--- Maak de tabel Rollercoaster
-CREATE TABLE Rollercoaster
-(
-Id                   SMALLINT     UNSIGNED    NOT NULL       AUTO_INCREMENT            COMMENT'Primary key of the Rollercoaster table'
-,Rollercoaster       VARCHAR(50)              NOT NULL                                 COMMENT 'Name of the rollercoaster'
-,AmusementPark       VARCHAR(50)              NOT NULL                                 COMMENT 'Name of the amusementpark'
-,Country             VARCHAR(50)              NOT NULL                                 COMMENT 'Country where it is located'
-,TopSpeed            SMALLINT     UNSIGNED    NOT NULL                                 COMMENT 'Topspeed in km/h'
-,Height              TINYINT      UNSIGNED    NOT NULL                                 COMMENT 'Height in meters'
-,YearOfConstruction  DATE                     NOT NULL                                 COMMENT 'Year of construction'
-,IsActive            BIT                      NOT NULL      DEFAULT 1                  COMMENT 'Indicates whether the rollercoaster is Active(1)'
-,Remark              VARCHAR(255)             NULL          DEFAULT NULL               COMMENT 'Optional remark or additional information'
-,DateCreated         DATETIME(6)              NOT NULL      DEFAULT NOW(6)             COMMENT 'Timestamp when the record was created'
-,DateChanged         DATETIME(6)              NOT NULL      DEFAULT NOW(6)             COMMENT 'Timestamp of the latest update'
-,CONSTRAINT          PK_Rollercoaster_Id      PRIMARY KEY (Id)
+-- Maak de tabel Rollercoaster aan
+CREATE TABLE Rollercoaster (
+    id                SMALLINT      UNSIGNED      NOT NULL AUTO_INCREMENT       COMMENT 'primary key of the Rollercoaster table'
+,Rollercoaster     VARCHAR(50)                 NOT NULL                      COMMENT 'name of the rollercoaster'
+,AmusementPark     VARCHAR(50)                 NOT NULL                      COMMENT 'name of the amusementpark'
+,CONSTRAINT        PK_Rollercoaster_Id         PRIMARY KEY (id)  
+,Country           VARCHAR(50) NOT NULL                                      COMMENT 'Country where it is located'
+,TopSpeed          SMALLINT UNSIGNED NOT NULL                                COMMENT 'Topspeed in km/h'
+,Height            TINYINT UNSIGNED NOT NULL                                 COMMENT 'Height in meters'
+,YearOfConstruction DATE NOT NULL                                            COMMENT 'Year of construction'
+,IsActive          BIT NOT NULL DEFAULT 1                                    COMMENT 'Indicates whether the rollercoaster is Active(1)'
+,Remark            VARCHAR(255) NULL DEFAULT NULL                            COMMENT 'Optional remark or additional information'
+,DateCreated       DATETIME(6) NOT NULL DEFAULT NOW(6)                       COMMENT 'Timestamp when the record was created'
+,DateChanged       DATETIME(6) NOT NULL DEFAULT NOW(6)                       COMMENT 'Timestamp of the latest update'
 ) ENGINE=InnoDB;
 
 -- Stap : 03
--- ----------------------------------------------------------------------
+-- **********************************************************
 -- Doel : Vul de tabel Rollercoaster met data
--- ----------------------------------------------------------------------
--- Versie Datum Autuer Omschrijving
--- ----------------------------------------------------------------------
--- -- 01 04-12-2025 F.Meulen Vul tabel hoogste
+-- **********************************************************
+-- Versie      Datum      Autuer       Omschrijving
+-- **********************************************************
+-- -- 01       04-12-2025 A.Akoudad    Vul tabel hoogste
 -- -- achtbanen van Europa
--- ----------------------------------------------------------------------
+-- **********************************************************
 
 -- Vul de tabel
 INSERT INTO Rollercoaster
@@ -65,10 +64,10 @@ Rollercoaster
 ,Height
 ,YearOfConstruction
 )
-
 VALUES
 ('Kingda Ka', 'Six Flags Great Adventure', 'Verenigd Koninkrijk', 206, 127, '2005-10-21')
 ,('Red Force', 'Ferrari Land', 'Spanje', 180, 112, '2017-04-07')
 ,('Hyperion', 'Energylandia', 'Polen', 142, 77, '2018-08-14')
 ,('Shambhala', 'PortAventura Park', 'Spanje', 134, 76, '2012-04-07')
 ,('Schwur des Karnen', 'Hansa Park', 'Duitsland', 127, 73, '2017-02-25');
+
---
+++ read.php
@@ -0,0 +1,92 @@
+<?php
+include('config/config.php');
+ 
+$dsn = "mysql:host=$dbHost;dbname=$dbName;charset=UTF8";
+$pdo = new PDO($dsn, $dbUser, $dbPass);
+ 
+$sql = "SELECT HAVE.Id
+              ,HAVE.Rollercoaster
+              ,HAVE.AmusementPark
+              ,HAVE.Country
+              ,HAVE.Topspeed
+              ,HAVE.Height
+              ,DATE_FORMAT(HAVE.YearOfConstruction, '%d-%m-%Y') AS YOFC
+        FROM rollercoaster AS HAVE
+        ORDER BY HAVE.Height DESC";
+ 
+$statement = $pdo->prepare($sql);
+$statement->execute();
+$result = $statement->fetchAll(PDO::FETCH_OBJ);
+?>
+ 
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CRUD-Basics</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+<body>
+    <div class="container mt-3">
+        <div class="row justify-content-center">
+            <div class="col-10">
+                <h2><u>Hoogste achtbanen van Europa</u></h2>
+            </div>
+        </div>
+ 
+        <div class="row justify-content-center my-3">
+    <div class="col-10">
+        <h5>
+            Nieuwe achtbaan
+            <a href="./create.php">
+                <i class="bi bi-plus-square text-danger"></i>
+            </a>
+        </h5>
+    </div>
+</div>
+ 
+        <div class="row justify-content-center mt-3">
+            <div class="col-10">
+                <table class="table table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th>Naam Achtbaan</th>
+                            <th>Naam Pretpark</th>
+                            <th>Land</th>
+                            <th>Topsnelheid (km/u)</th>
+                            <th>Hoogte (m)</th>
+                            <th>Bouwjaar</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($result as $rollercoaster): ?>
+                            <tr>
+                                <td><?= $rollercoaster->Rollercoaster; ?></td>
+                                <td><?= $rollercoaster->AmusementPark; ?></td>
+                                <td><?= $rollercoaster->Country; ?></td>
+                                <td><?= $rollercoaster->Topspeed; ?></td>
+                                <td><?= $rollercoaster->Height; ?></td>
+                                <td><?= $rollercoaster->YOFC; ?></td>
+                                <td class="text-center">
+    <a href="update.php?id=<?= $rollercoaster->Id; ?>">
+        <i class="bi bi-pencil-square text-success"></i>
+    </a>
+</td>
+ 
+                                <td>
+                                    <a href="delete.php?id=<?= $rollercoaster->Id; ?>">
+                                        <i class="bi bi-x-square text-danger"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
